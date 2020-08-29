532e38b504fa412fea2187c7f82885c07140c592
oleometal <oleometal@gmail.com>
2020-08-28 18:04:45 -0600

    u

--- guiones/índice.js
+++ guiones/índice.js
@@ -1,1 +1,24 @@
-console.log("hola");
+"use strict";
+
+    let updateButton = document.getElementById('updateDetails');
+    let favDialog = document.getElementById('favDialog');
+    let outputBox = document.querySelector('output');
+    let selectEl = document.querySelector('select');
+    let confirmBtn = document.getElementById('confirmBtn');
+
+    // "Update details" el botón abre el modal <dialog>
+    updateButton.addEventListener('click', function onOpen() {
+        if(typeof favDialog.showModal === "function"){
+            favDialog.showModal();
+        }else{
+            alert("La API <dialog> no es soportada por este navegador");
+        }
+    });
+    // "Favorite animal" input sets the value of the submit button
+selectEl.addEventListener('change', function onSelect(e) {
+    confirmBtn.value = selectEl.value;
+  });
+    // "Confirm" este botón dispara el evento "close" en <dialog> debido a [method="dialog"]
+    favDialog.addEventListener('close', function onClose(){
+        outputBox.value = favDialog.returnValue + " botón apretado - " + (new Date()).toString();
+    });
